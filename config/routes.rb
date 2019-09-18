858681a9ff93784c65d1a841d541281928f936ee
Evelina Larsson <aevelinalarsson@gmail.com>
2019-09-18 18:55:19 +0100

    Webpack popper jquery bootstrap added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
+  get 'about', to: 'pages#about', as: :about
+  get 'experience', to: 'pages#experience', as: :experience
+  get 'projects', to: 'pages#projects', as: :projects
+  get 'contact', to: 'pages#contact', as: :contact
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
