4936d39f178f33795c60079dedcd26294bf4b2a4
Evelina Larsson <aevelinalarsson@gmail.com>
2019-09-19 17:26:55 +0100

    jquery added for dropdown hover instead of click. Daniel's background image added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,12 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-import 'bootstrap';
+
+$(document).ready(function(){
+  $(".dropdown, .btn-group").hover(function(){
+    var dropdownMenu = $(this).children(".dropdown-menu");
+    if(dropdownMenu.is(":visible")){
+      dropdownMenu.parent().toggleClass("open");
+    }
+  });
+});
